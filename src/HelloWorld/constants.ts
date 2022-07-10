51da9f7eeebd40095de7469efb2b68e47f9c7ea1
3salaz <3salaz.dev@gmail.com>
2022-07-10 04:13:12 +0000

    initial commit

--- src/HelloWorld/constants.ts
+++ src/HelloWorld/constants.ts
@@ -1,6 +1,6 @@
 // Change any of these to update your video live.
 
-export const COLOR_1 = '#86A8E7';
-export const COLOR_2 = '#91EAE4';
+export const COLOR_1 = '#FFFFF';
+export const COLOR_2 = '#FFFFF';
 
 export const FONT_FAMILY = 'SF Pro Text, Helvetica, Arial, sans-serif';
